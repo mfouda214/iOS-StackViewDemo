3c2e6e48c70b7d1f217b2e9ba2ce4e1afcebfb9c
mfouda214 <m.fouda@robone.net>
2018-03-17 22:29:32 +0000

    initail commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Mohamed Sobhi  Fouda on 3/17/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
